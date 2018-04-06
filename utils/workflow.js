979c1802bbd933842e2457e0eb9a2c26e87100cf
kohsah <ashok@hariharan.org.in>
2018-04-07 02:11:09 +0530

    Adding additional comments to code

--- utils/workflow.js
+++ utils/workflow.js
@@ -1,8 +1,22 @@
 const workflow = require('gawati-workflow');
 const path = require('path');
 
+/**
+ * We call the disoverSync api here to load the workflows
+ * This is called at startup time and not at runtime, and so is non-blocking, despite
+ * being a synchronous API
+ */
 const wf = workflow.discoverSync(path.join(".", "workflow_configs"))
 
+/**
+ * Returns the state information for a workflow - for a specified docType and subType 
+ * comboination.  Expects the current state, and an array containing loaded workflows
+ * as additional inputs.
+ * @param {*} docAknType 
+ * @param {*} subtype 
+ * @param {*} curWFState 
+ * @param {*} wfArr 
+ */
 const getWFStateInfo = (docAknType, subtype, curWFState, wfArr) => {
     //Get the relevant Workflow config for the current doc type and subtype
     var relevantWF = wfArr.filter(function (el) {
