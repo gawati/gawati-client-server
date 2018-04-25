9fe9c3e68fca97073fe1536e0f3f8b20a19a36a5
ashwinibm <ashwinibm88@gmail.com>
2018-04-25 11:43:30 +0530

    Send auth config to client

--- apiroutes.js
+++ apiroutes.js
@@ -56,7 +56,6 @@ router.post("/document/auth",
     ]
 );
 
-
 function terminal(req, res) {
     res.json({msg:"Completed !", auth: res.locals.gawati_auth});
 }
@@ -73,6 +72,11 @@ router.get(
         res.status(200).send(aboutInfo);
     }
 );
+
+// Send the keycloak config file
+router.get('/auth/config', function (req, res) {
+  res.send(authJSON);
+})
   
 
 module.exports = router;
