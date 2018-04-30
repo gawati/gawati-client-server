d5720fcbe5a85dc88998a9da4a44d617080b7de2
ashwinibm <ashwinibm88@gmail.com>
2018-04-30 18:39:05 +0530

    Remove dead code

--- attapis.routes.js
+++ attapis.routes.js
@@ -3,18 +3,15 @@ const attapis = require("./attapis");
 var attAPIs  = {};
 const BASE_URI = "/attachments"
 
-/*
-Creates or Updates an attachment.
-Input form data submitted to the API:
-  data: 
-    - index
-    - file
-    - fileName
-    - title
-    - fileType
-    - iri
-    - All the fields of pkgIdentity
-    - pkgAttachments
+/* 
+* Creates or Updates an attachment.
+* Receive the form data containing the file info and the parent document info.
+* Write the file info to the file system.
+* Add/Update attachment to the attachments list.
+* Save the new attachment list to the Database.
+* Input form data submitted to the API:
+*   index, file, fileName, title, fileType, iri, pkgAttachments, and
+*   all pkgIdentity fields.
 */
 attAPIs[`${BASE_URI}/upload`] = {
     method: "post", 
@@ -28,7 +25,12 @@ attAPIs[`${BASE_URI}/upload`] = {
 };
 
 /*
-Removes an attachment from the filesystem and the xml on exist-db.
+* Removes an attachment.
+* Receive the JSON containing the attachment info and the document info.
+* Remove attachment from the FS
+* Remove attachment from attachments list.
+* Save the new attachment list to the Database.
+*/
 Input json object submitted to the API:
 {
     "data": {
