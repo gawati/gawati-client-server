f2ec5c30ecbd3b1898b1a08fce00c957f1a379c9
Arunadevi <l.arunadevi@gmail.com>
2018-08-09 13:29:30 +0530

    Use attachment path from configuration

--- thumbnails.js
+++ thumbnails.js
@@ -27,13 +27,16 @@ const walkSync = function(dir, filelist) {
  */
 function createThumbnails() {
     console.log("Thumbnails:");
-    const filelist = walkSync('akn_data', []);
+    const filelist = walkSync(appconstants.AKN_ATTACHMENTS(), []);
     filelist.forEach(function (file, i) {
         const pdfImage = new PDFImage (file);
         if (fs.existsSync(file)) {
-            console.log("converting" + file);
+            console.log("Converting " + file);
             pdfImage.convertPage(0).then(function (imagePath) {
-                console.log("Thumbnail generated: " + i + " of " + filelist.length + " - " + imagePath);
+                console.log("Thumbnail generated: " + (i + 1) + " of " + filelist.length + " - " + imagePath);
+                if (i == filelist.length - 1) {
+                    console.log("***End of Thumbnail Job***");
+                }
             }), function (err) {
                 console.log(err);
             };
@@ -42,18 +45,5 @@ function createThumbnails() {
           console.log("Not found");
     })
 }
-
-// function createThumbnails () {
-//     const file = "E:/gawati/gawati-editor-fe/akn_data/akn/ao/act/legge/2018-06-21/1/ara@/akn_ao_act_legge_2018-06-21_1_ara_main_1.pdf";
-//     var pdfImage = new PDFImage(file);
-// console.log("Start");
-// pdfImage.convertPage(0).then(function (imagePath) {
-//   // 0-th page (first page) of the slide.pdf is available as slide-0.png
-//   console.log("Converted.");
-//   fs.existsSync("akn_data/akn/ao/act/legge/2018-06-21/1/ara@/akn_ao_act_legge_2018-06-21_1_ara_main_1-0.png") // => true
-// }, function (err) {
-//       console.log(err);
-// });
-// };
 
 module.exports.createThumbnails = createThumbnails;
